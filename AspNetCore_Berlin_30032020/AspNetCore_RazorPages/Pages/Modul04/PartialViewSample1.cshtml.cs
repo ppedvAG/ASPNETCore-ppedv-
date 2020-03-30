cdf0b907149c449bccbb7635638652fa7ab284ea
Kevin Winter <KevinW@ppedv.de>
2020-03-30 16:24:28 +0200

    PartialViewSample1.cshtml PartialViewSample1.cshtml.cs _Excel.cshtml _Zeit.cshtml

---
+++ AspNetCore_Berlin_30032020/AspNetCore_RazorPages/Pages/Modul04/PartialViewSample1.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCore_RazorPages.Pages.Modul04
+{
+    public class PartialViewSample1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
